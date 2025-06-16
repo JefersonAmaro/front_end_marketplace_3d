89b43b30a0ed5b765239ffc8391adb1a194a588e
Guilherme Betsa <ybtyza@gmail.com>
2025-06-16 12:30:30 -0300

    feat: 🎸 (MarketPlace) Desenvolvimento SectionThree

--- src/pages/marketplace/index.jsx
+++ src/pages/marketplace/index.jsx
@@ -1,11 +1,13 @@
 import SectionOne from "../../components/marketplace/sectionOne"
 import SectionTwo from "../../components/marketplace/sectionTwo"
+import SectionThree from "../../components/marketplace/sectionThree"
 
 function Marketplace () {
     return (
         <>
             <SectionOne />
             <SectionTwo />
+            <SectionThree />
         </>
     )
 }
