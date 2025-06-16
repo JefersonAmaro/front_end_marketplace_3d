a50eb68412550810d7abe4b7bcac098246e9cfe6
Guilherme Betsa <ybtyza@gmail.com>
2025-06-16 11:14:26 -0300

    feat: 🎸 (SectionTwo) Desenvolvimento dos Cards

--- src/pages/marketplace/index.jsx
+++ src/pages/marketplace/index.jsx
@@ -1,9 +1,11 @@
 import SectionOne from "../../components/marketplace/sectionOne"
+import SectionTwo from "../../components/marketplace/sectionTwo"
 
 function Marketplace () {
     return (
         <>
             <SectionOne />
+            <SectionTwo />
         </>
     )
 }
