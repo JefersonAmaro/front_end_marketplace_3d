a4b1da1a7edfa22a30b96485d09bbb24c38dcead
Guilherme Betsa <ybtyza@gmail.com>
2025-08-11 15:48:18 -0300

    feat: 🎸 (Integração com sistema de Login e Cadastro backend)

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -4,11 +4,102 @@ import { createContext, useState, useEffect } from "react";
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  async function loginUser(loginData) {
+    setLoading(true);
+    try {
+      const response = await axios.post("http://localhost:3000/auth/user", {
+        email: loginData.email,
+        password: loginData.senha,
+      });
+
+      setLoading(false);
+      return response.status;
+    } catch (error) {
+      setErrorMessage(error.response?.data.message);
+      setLoading(false);
+    }
+  }
+
+  async function loginSupplier(loginData) {
+    setLoading(true);
+    try {
+      const response = await axios.post("http://localhost:3000/auth/supplier", {
+        email: loginData.email,
+        password: loginData.senha,
+      });
+
+      setLoading(false);
+      return response.status;
+    } catch (error) {
+      setErrorMessage(error.response?.data.message);
+      setLoading(false);
+    }
+  }
+
+  async function registerUser(user) {
+    setLoading(true);
+
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/register/user",
+       {  
+        name: user.nome,
+        email: user.email,
+        telefone: user.tel,
+        endereco: user.endereco,
+        password: user.senha
+       }
+      );
+
+      console.log(response);
+
+      setLoading(false);
+      return response.status;
+    } catch (error) {
+      setErrorMessage(error.response?.data.message);
+      setLoading(false);
+    }
+  }
+
+  async function registerSupplier(supplier) {
+    setLoading(true);
+
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/register/supplier",
+        {
+          name: supplier.nome,
+          email: supplier.email,
+          telefone: supplier.tel,
+          endereco: supplier.endereco,
+          cpf_cnpj: supplier.cpfCnpj,
+          password: supplier.senha
+        }
+      );
+
+      setLoading(false);
+      return response.status;
+    } catch (error) {
+      setErrorMessage(error.response?.data.message);
+      setLoading(false);
+    }
+  }
 
   return (
-    <AuthContext.Provider value={{ data, loading }}>
+    <AuthContext.Provider
+      value={{
+        loading,
+        loginUser,
+        loginSupplier,
+        registerUser,
+        registerSupplier,
+        errorMessage,
+        setErrorMessage,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,12 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routers/routers.jsx";
 
 import { DataContextProvider } from "./context/dataContext.jsx";
+import { AuthContextProvider } from "./context/authContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <DataContextProvider>
-    <RouterProvider router={router} />
+    <AuthContextProvider>
+      <RouterProvider router={router} />
+    </AuthContextProvider>
   </DataContextProvider>
 );
